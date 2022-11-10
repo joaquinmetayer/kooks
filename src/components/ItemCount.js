e4581e9fa3f95ad67ef636249a1f5de4fe123c8b
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-11-10 17:22:16 -0300

    406

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -5,32 +5,27 @@ export default function ItemCount() {
   let [toCart, setToCart] = useState(1);
   let [stock, setStock] = useState(9);
 
+  function itemCountCalc(str){
+    if(str === 'sum' && toCart >= 1 && toCart < 10){
+      setStock(stock - 1);
+      setToCart(toCart + 1);
+    }else if (str === 'res' && toCart > 1 && toCart < 11){
+      setStock(stock + 1);
+      setToCart(toCart - 1);
+    }
+  }
   return (
     <div className="itemCount">
       <span
         className="btnItemSR"
-        onClick={() => {
-          if (toCart > 1) {
-            setStock(stock + 1);
-            setToCart(toCart - 1);
-            console.log(stock);
-            console.log(toCart);
-          }
-        }}
+        onClick={() => itemCountCalc('res')}
       >
         -
       </span>
       <span className="toCart"> {toCart} </span>
       <span
         className="btnItemSR"
-        onClick={() => {
-          if (stock !== 0) {
-            setStock(stock - 1);
-            setToCart(toCart + 1);
-            console.log(stock);
-            console.log(toCart);
-          }
-        }}
+        onClick={() => itemCountCalc('sum')}
       >
         +
       </span>
